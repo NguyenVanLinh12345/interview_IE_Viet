2f3cd324c2f92e76dc4db0be1bd940aa735769d4
Linhnv1 <linhnv1@bsscomerce.com>
2025-06-13 08:18:03 +0700

    Update port

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,7 @@ app.use('/', indexRoutes);
 app.use('/api/owner', ownerRoutes);
 app.use('/api/employee', employeeRoutes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
