72bec115b3a72db3d6197997e83f864455e6153a
Linhnv1 <linhnv1@bsscomerce.com>
2025-06-13 10:16:47 +0700

    update

--- backend/app.js
+++ backend/app.js
@@ -1,19 +1,48 @@
 require('dotenv').config();
+const PORT = process.env.PORT || 3001;
+
 const express = require('express');
-const app = express();
+const cors = require('cors');
 
 // Import routers
 const indexRoutes = require('./routes/index');
 const ownerRoutes = require('./routes/owner');
 const employeeRoutes = require('./routes/employee');
 
-app.use(express.json());
+const app = express();
+const http = require('http').Server(app);
 
+app.use(express.json());
+app.use(cors());
 app.use('/', indexRoutes);
 app.use('/api/owner', ownerRoutes);
 app.use('/api/employee', employeeRoutes);
 
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => {
+const socketIO = require('socket.io')(http, {
+    cors: {
+        origin: "http://localhost:3000"
+    }
+});
+
+//Add this before the app.get() block
+socketIO.on('connection', (socket) => {
+    console.log(`⚡: ${socket.id} user just connected!`);
+    console.log(socket.data)
+    
+    socket.on('disconnect', () => {
+        console.log('🔥: A user disconnected');
+    });
+
+    socket.on('message', (data) => {
+        console.log(data)
+    });
+
+    socket.on('join room', (data) => {
+        console.log('join room', data)
+    });
+});
+
+
+http.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
--- frontend/src/components/common/message/ChatBoard.tsx
+++ frontend/src/components/common/message/ChatBoard.tsx
@@ -1,11 +1,55 @@
+'use client'
+
 import { Avatar, BlockStack, Box, Card, InlineStack, Layout, Text } from "@shopify/polaris";
 import ChatContent from "./ChatContent";
+import { useEffect, useRef } from "react";
+
+import { io, Socket } from 'socket.io-client';
+
+interface ServerToClientEvents {
+    'chat message': (msg: string) => void;
+    'user joined': (username: string) => void;
+}
+
+interface ClientToServerEvents {
+    'send message': (msg: string) => void;
+    'join room': (username: string) => void;
+}
+
+// Example usage
+// sendMessage('Hello server!');
 
 export default function ChatBoard() {
+    const socketRef = useRef<Socket<ServerToClientEvents, ClientToServerEvents> | undefined>(undefined);
 
     const handleSend = async (content: string) => {
         console.log(content);
     }
+
+    useEffect(() => {
+        socketRef.current = io('http://localhost:3001');
+
+        // Event listener for connection
+        socketRef.current?.on('connect', () => {
+            console.log('Connected to server');
+            // Emit join event with user name
+            socketRef.current?.emit('join room', 'user123');
+        });
+
+        // Event listener for 'chat message'
+        socketRef.current?.on('chat message', (msg) => {
+            console.log('Message:', msg);
+        });
+
+        // Event listener for 'user joined'
+        socketRef.current?.on('user joined', (username) => {
+            console.log(`${username} has joined the chat.`);
+        });
+
+        return () => {
+            socketRef.current?.disconnect();
+        }
+    }, [])
 
     return (
         <Box padding={'400'}>
